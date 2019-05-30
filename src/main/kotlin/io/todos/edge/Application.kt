6ed9c2a2d19b088e3f3071196ddfc147adb6f5c7
corbs <corbsmartin@gmail.com>
2019-05-30 10:19:42 -0500

    cleaning up non-essentials

--- src/main/kotlin/io/todos/edge/Application.kt
+++ src/main/kotlin/io/todos/edge/Application.kt
@@ -2,26 +2,11 @@ package io.todos.edge
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.context.config.annotation.RefreshScope
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.web.server.SecurityWebFilterChain
 
-@EnableDiscoveryClient
 @SpringBootApplication
-@RefreshScope
 @Configuration
-@EnableWebFluxSecurity
-class Application {
-
-    @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        return http.authorizeExchange().anyExchange().permitAll().and().httpBasic().disable().csrf().disable().build()
-    }
-}
+class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
